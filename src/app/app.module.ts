dac44e33ff39302e0b860cf7b0e9fcba7678efef
Antony Baasandorj <antony.baasan@gmail.com>
2019-01-09 09:01:37 -0500

    add header'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
     AppComponent
   ],
   imports: [
